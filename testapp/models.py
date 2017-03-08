aec136c3a95b0bd2f87b81bdddbcd96354103fcd
SanjuktaG <sanjukta.ganguli@ebp-global.com>
2017-03-08 11:06:34 +0800

    branch update test

--- testapp/models.py
+++ testapp/models.py
@@ -14,3 +14,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
